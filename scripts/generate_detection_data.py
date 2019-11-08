86f17027fbcfb892b665666363c9ea3b74d3d11f
erickkramer <erickkramer@gmail.com>
2019-11-08 19:34:57 +0100

    Added the generation of the images and masks directories

--- scripts/generate_detection_data.py
+++ scripts/generate_detection_data.py
@@ -7,10 +7,12 @@ from dataset_interface.augmentation.image_augmentation import ImageAugmenter
 
 def generate_masks_and_annotations(data_dir, background_dir, class_annotation_file,
                                    num_objects_per_class, output_dir, output_annotation_dir,
-                                   display_boxes):
+                                   display_boxes, invert_mask):
     augmenter = ImageAugmenter(data_dir, background_dir, class_annotation_file, num_objects_per_class)
     if not output_dir:
         output_dir = os.path.join(data_dir, 'synthetic_images')
+        output_dir_images = os.path.join(output_dir, 'images')
+        output_dir_masks = os.path.join(output_dir, 'masks')
     if not output_annotation_dir:
         output_annotation_dir = os.path.join(data_dir, 'annotations')
     TerminalColors.formatted_print("begin generating images under '{}' and annotation files under '{}'"
@@ -18,6 +20,13 @@ def generate_masks_and_annotations(data_dir, background_dir, class_annotation_fi
     if not os.path.isdir(output_dir):
         print("creating directory: " + output_dir)
         os.mkdir(output_dir)
+    if not os.path.isdir(output_dir_images):
+        print("creating directory: " + output_dir_images)
+        os.mkdir(output_dir_images)
+    if not os.path.isdir(output_dir_masks):
+        print("creating directory: " + output_dir_masks)
+        os.mkdir(output_dir_masks)
+        
     if not os.path.isdir(output_annotation_dir):
         print("creating directory: " + output_annotation_dir)
         os.mkdir(output_annotation_dir)
@@ -36,8 +45,8 @@ def generate_masks_and_annotations(data_dir, background_dir, class_annotation_fi
 
         max_obj_num_per_bg = int(prompt_for_float("enter the maximum number of objects per background"))
         # generate images
-        augmenter.generate_detection_data(split_name, output_dir, output_annotation_dir, max_obj_num_per_bg, 
-            display_boxes=display_boxes)
+        augmenter.generate_detection_data(split_name, output_dir_images, output_dir_masks, output_annotation_dir, max_obj_num_per_bg, 
+            display_boxes=display_boxes, invert_mask=invert_mask)
 
         if not prompt_for_yes_or_no("do you want to generate images for another dataset split?"):
             break
@@ -63,12 +72,14 @@ if __name__ == '__main__':
                         help='(optional) directory to store the generated YAML annotations')
     parser.add_argument('--display-boxes', '-b', action='store_true',
                         help='(optional) whether to display the synthetic images with visualized bounding boxes')
+    parser.add_argument('--invert_mask', '-im', action='store_true',
+                        help='wheter to invert the mask (Required for YCB)')
     args = parser.parse_args()
 
     try:
         generate_masks_and_annotations(args.data_directory, args.background_directory, args.class_annotations,
                                        args.num_objects_per_class, args.output_dir,
-                                       args.output_annotation_dir, args.display_boxes)
+                                       args.output_annotation_dir, args.display_boxes, args.invert_mask)
         TerminalColors.formatted_print('image and annotation generation complete', TerminalColors.OKGREEN)
     except KeyboardInterrupt:
         TerminalColors.formatted_print('\nscript interrupted', TerminalColors.WARNING)
