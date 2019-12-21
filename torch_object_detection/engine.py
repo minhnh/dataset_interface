98ca7cfe7d23e8b791a27276690815ffb664c423
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2019-12-21 12:31:01 +0100

    [torch_detector/engine] Added a validation fn

--- torch_object_detection/engine.py
+++ torch_object_detection/engine.py
@@ -52,6 +52,33 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, lo
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
 
+def validate_one_epoch(model, data_loader, device, epoch, print_freq, loss_file_name=None):
+    model.train()
+    metric_logger = utils.MetricLogger(delimiter="  ")
+
+    header = '------------------------------'
+    header += 'Epoch: [{}]'.format(epoch)
+
+    for images, targets in metric_logger.log_every(data_loader, print_freq, header, loss_file_name):
+        images = list(image.to(device) for image in images)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+        loss_dict = model(images, targets)
+
+        losses = sum(loss for loss in loss_dict.values())
+
+        # reduce losses over all GPUs for logging purposes
+        loss_dict_reduced = utils.reduce_dict(loss_dict)
+        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+
+        loss_value = losses_reduced.item()
+
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            print(loss_dict_reduced)
+            sys.exit(1)
+
+        metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
 
 def _get_iou_types(model):
     model_without_ddp = model
