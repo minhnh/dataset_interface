db28914d55ca13308cdf9c27117275abe00bfd8b
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2020-02-25 09:50:27 +0100

    [utils] Added conditional removal of ROS python packages from the path
    This is added as in various other scripts in the repository due to
    ROS-related problems with OpenCV

--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
 from builtins import input      # for Python 2 compatibility
+
+import sys
+try:
+    sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+except:
+    pass
+
 import argparse
 import glob
 import os
